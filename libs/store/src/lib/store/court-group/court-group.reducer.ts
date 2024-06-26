5e0172e328f24ef5ab78668f972ec71a8dbe8804
mkhoi15 <khoivmse171254@fpt.edu.vn>
2024-06-26 21:44:20 +0700

    Add Reducer. Done reload the list after create

--- libs/store/src/lib/store/court-group/court-group.reducer.ts
+++ libs/store/src/lib/store/court-group/court-group.reducer.ts
@@ -1,28 +1,31 @@
 import { createReducer, on } from '@ngrx/store';
 import * as CourtGroupActions from './court-group.action';
 import { CourtGroup } from './court-group.model';
-import { state } from '@angular/animations';
-import { error } from 'ng-packagr/lib/utils/log';
+
 
 export interface CourtGroupState {
   courtGroups: CourtGroup[];
   error: string | null;
+  courtGroupCreated: boolean;
 }
 
 export const courtGroupInitialState: CourtGroupState = {
   courtGroups: [],
-  error: null
+  error: null,
+  courtGroupCreated: false
 };
 
 export const courtGroupReducer = createReducer(
   courtGroupInitialState,
   on(CourtGroupActions.loadCourtGroupsSuccess, (state, { courtGroups }) => ({
     ...state,
-    courtGroups: courtGroups
+    courtGroups: courtGroups,
+    courtGroupCreated: false
   })),
   on(CourtGroupActions.loadCourtGroupsFailure, (state, { error }) => ({
     ...state,
-    error
+    error,
+    courtGroupCreated: false
   })),
   on(CourtGroupActions.loadCourtGroupByOwnerId, (state) => ({
     ...state,
@@ -31,19 +34,23 @@ export const courtGroupReducer = createReducer(
   on(CourtGroupActions.createCourtGroupSuccess, (state, { courtGroup }) => ({
     ...state,
     courtGroups: [...state.courtGroups, courtGroup],
-    error: null
+    error: null,
+    courtGroupCreated: true
   })),
   on(CourtGroupActions.createCourtGroupFailure, (state, { error }) => ({
     ...state,
-    error
+    error,
+    courtGroupCreated: false
   })),
   on(CourtGroupActions.searchCourtGroupsSuccess, (state, { courtGroups }) => ({
     ...state,
     courtGroups,
-    error: null  // Reset the error on success
+    error: null,  // Reset the error on success
+    courtGroupCreated: false
   })),
   on(CourtGroupActions.searchCourtGroupsFailure, (state, { error }) => ({
     ...state,
-    error
+    error,
+    courtGroupCreated: false
   }))
 );
